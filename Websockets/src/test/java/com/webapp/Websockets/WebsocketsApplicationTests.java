110b41845d3ca1100e30f7a11a74e09875921c99
harrybro <hariomthakur638@gmail.com>
2026-04-07 22:44:40 +0530

    project initialized with dependencies

---
+++ Websockets/src/test/java/com/webapp/Websockets/WebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.Websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
