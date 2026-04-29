7c6f1ad2230cdcdbe844b6376695eafa0c766136
harrybro <hariomthakur638@gmail.com>
2026-04-29 20:49:50 +0530

    entities initialized

---
+++ ChatingAppBackend/src/main/java/com/Project/ChatingAppBackend/entities/Message.java
@@ -0,0 +1,16 @@
+package com.Project.ChatingAppBackend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Message {
+
+    private String
+}
