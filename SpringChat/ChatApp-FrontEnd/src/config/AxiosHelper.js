1db97977bcb9fb02995e247de2d028c240e962c4
harrybro <hariomthakur638@gmail.com>
2026-05-07 22:33:35 +0530

    services initialized successfully in order to make HTTP requests and get responses.

---
+++ SpringChat/ChatApp-FrontEnd/src/config/AxiosHelper.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+export const API_URL = 'http://localhost:8080';
+export const httpClient = axios.create({
+    baseURL: API_URL
+});
---
+++ SpringChat/ChatApp-FrontEnd/src/services/RoomService.js
@@ -0,0 +1,7 @@
+import { httpClient } from "../config/AxiosHelper";
+export const createRoom = async(roomDetail)=>{
+
+   const response =  await httpClient.post(`/api/v1/rooms`,roomDetail);
+   return response.data;
+   
+}
